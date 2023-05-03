74b5c72914fd5a00f9623b41ebaf95537e10d2c3
Sam Merham <sam@smerham.com>
2023-05-03 11:12:50 -0700

    added another test using it:

--- src/components/__tests__/example.spec.js
+++ src/components/__tests__/example.spec.js
@@ -1,3 +1,11 @@
 // sanity test --->> test to check our tools are working
 // import { test } from "vitest";
-test();
+// https://vitest.dev/api/#expect
+test("sanity test", () => {
+  expect(true).toBe(true);
+});
+
+// same as below
+it("sanity test", () => {
+  expect(1).toBe(1);
+});
