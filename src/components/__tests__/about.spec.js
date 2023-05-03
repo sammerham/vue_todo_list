0faa6924f098773c7d2179dcfa14f28665a4e34e
Sam Merham <sam@smerham.com>
2023-05-03 12:02:17 -0700

    added mount and shallow mount

--- src/components/__tests__/about.spec.js
+++ src/components/__tests__/about.spec.js
@@ -1,10 +1,16 @@
 // // same as below
 import { mount } from "@vue/test-utils";
 
+// import { shallowMount } from "@vue/test-utils"; // limit
+// mount will load as many children as it wants.  while shallow will limit children to one level of children component;
 import About from "@/views/About.vue";
 describe("About.vue", () => {
   test("renders inner text", () => {
-    const wrapper = mount(About);
+    // const wrapper = shallowMount(About);
+    const wrapper = mount(About, {
+      shallow: true,
+    });
+    console.log("wrapper--->>", wrapper.text());
     expect(wrapper.text()).toContain("about");
   });
 });
