3aa90b1d00b34d81cdd4a855b09fbfe2d1c6af9f
Sam Merham <sam@smerham.com>
2023-05-04 10:23:59 -0700

    writing more test cases of how component interact with pinia store

--- src/components/__tests__/todo.spec.js
+++ src/components/__tests__/todo.spec.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
+import { createTestingPinia } from "@pinia/testing";
 import { shallowMount } from "@vue/test-utils";
 
 import Todo from "@/components/Todo.vue";
+import useTodosStore from "@/stores/todo";
 
 describe("Todo.vue", () => {
   test("render todo item", () => {
@@ -10,11 +13,21 @@ describe("Todo.vue", () => {
       completed: false,
     };
     const wrapper = shallowMount(Todo, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            // createSpy: vitest.fn,
+            createSpy: vi.fn,
+          }),
+        ],
+      },
       propsData: {
         // data coming from prop
         todo,
       },
     });
+    // eslint-disable-next-line no-unused-vars
+    const store = useTodosStore();
     expect(wrapper.text()).toContain(todo.task);
     const li = wrapper.find(".incomplete");
 
@@ -22,5 +35,17 @@ describe("Todo.vue", () => {
 
     expect(li.classes()).toContain("incomplete");
     expect(li.text()).toBe("test123");
+    expect(wrapper.find("div").findAll("ahi-button").length).toEqual(2);
+    const buttons = wrapper.find("div").findAll("ahi-button");
+    const [deleteBtn, editBtn] = buttons;
+    deleteBtn.trigger("click");
+    expect(store.deleteTodo).toHaveBeenCalledTimes(1);
+    expect(wrapper.find("div").findAll("ahi-icon-button").length).toEqual(2);
+    // check that the 'Clear Weather Data' button is displayed
+    // expect(wrapper.findAll("button").length).toEqual(2);
+    // expect(wrapper.findAll("button")[0].isVisible()).toBeTruthy();
+    // expect(wrapper.findAll("button")[0].text()).toMatch(
+    //   "Clear Weather Data (2)"
+    // );
   });
 });
